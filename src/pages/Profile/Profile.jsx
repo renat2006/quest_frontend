c039a5d2dc51f8bd7f192a05f44a0d06b7519508
Renat <70426913+renat2006@users.noreply.github.com>
2024-06-25 22:55:11 +0300

    Added toml file and some styes

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,16 +3,16 @@ import {Card, CardFooter, CardHeader, Image} from "@nextui-org/react";
 
 const Profile = () => {
     return (<div className="profile--container">
-        <Card isFooterBlurred className="w-full max-w-[400px] h-[450px] col-span-12 sm:col-span-7">
+        <Card isFooterBlurred isHea className="w-full max-w-[400px] h-[450px] col-span-12 sm:col-span-7">
             <CardHeader className="absolute z-10 top-1 flex-col items-start">
-                <p className="text-tiny text-white/60 uppercase font-bold">New</p>
-                <h4 className="text-black font-medium text-2xl">Acme camera</h4>
+                <p className="text-[0.6rem] text-black/70 uppercase font-bold">Будь ближе к сотням увлекательных квестов!</p>
+                <h4 className="text-black font-medium text-2xl">Войди через телеграм</h4>
             </CardHeader>
             <Image
                 removeWrapper
                 alt="Card example background"
                 className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-                src="https://nextui.org/images/card-example-6.jpeg"
+                src="https://yurmino.ru/wp-content/uploads/2021/09/scale_1200-4.jpg"
             />
             <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
                 <div>
