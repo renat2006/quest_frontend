71d0df1a1cc0c9f60459cf36962d65cb8ed0cf5a
Renat <70426913+renat2006@users.noreply.github.com>
2024-06-29 21:02:18 +0300

    Fixed some stranges

--- src/componets/MainPageQuestCard/MainPageQuestCard.jsx
+++ src/componets/MainPageQuestCard/MainPageQuestCard.jsx
@@ -2,7 +2,7 @@ import {Card, CardBody, CardFooter, CardHeader, Image} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faLocationDot, faStar} from "@fortawesome/free-solid-svg-icons";
 
-const MainPageQuestCard = ({ title, author, img, author_img, description, type, location, cost, rate, rate_sum }) => {
+const MainPageQuestCard = ({title, author, img, author_img, description, type, location, cost, rate, rate_sum}) => {
     return (
         <Card isPressable isFooterBlurred shadow="none"
               onPress={() => console.log("Карточка нажата")}
@@ -19,7 +19,8 @@ const MainPageQuestCard = ({ title, author, img, author_img, description, type, 
                     src={img}
                 />
             </CardBody>
-            <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+            <CardFooter
+                className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
                 <div className="flex flex-grow gap-2">
                     <Image
                         alt="Лого автора квеста"
@@ -30,16 +31,18 @@ const MainPageQuestCard = ({ title, author, img, author_img, description, type, 
                         <p className="text-sm text-white/70 line-clamp-1 font-medium indent-0">{description}</p>
                         <p className="text-tiny font-thin text-white/50 line-clamp-1">{type}</p>
                         <p className="text-tiny text-white/70 ">
-                            <FontAwesomeIcon icon={faLocationDot} className="text-blue-500"/>
-                            <span id="quest--location">{location}</span>
+                            <FontAwesomeIcon icon={faLocationDot} className="text-blue-500"/> <span
+                            id="quest--location">{location}</span>
+
                         </p>
                     </div>
                     <div className="flex flex-col items-end">
                         <p className="text-sm text-green-600 font-medium">{cost === "0" ? "Бесплатно" : `${cost} руб.`}</p>
                         <p className="text-tiny text-white/60">
-                            <FontAwesomeIcon icon={faStar} className="text-yellow-500"/>
-                            <span id="quest--rate">{rate}</span>
-                            <span id="quest--rate-sum">({rate_sum})</span>
+                            <FontAwesomeIcon icon={faStar} className="text-yellow-500"/> <span
+                            id="quest--rate">{rate}</span> <span id="quest--rate-sum">({rate_sum})</span>
+
+
                         </p>
                     </div>
                 </div>
