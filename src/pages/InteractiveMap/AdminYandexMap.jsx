beac4be2fee6f9855c65a1189a65615190fc79ed
renat2006 <renatgubaudullin@gmail.com>
2024-07-15 17:41:57 +0300

    Added yandex map integration

--- src/pages/InteractiveMap/AdminYandexMap.jsx
+++ src/pages/InteractiveMap/AdminYandexMap.jsx
@@ -1,27 +1,46 @@
-import React from "react";
-import {
-    YMap,
-    YMapDefaultSchemeLayer,
-    YMapDefaultFeaturesLayer, YMapComponentsProvider, YMapDefaultMarker, YMapMarker
+import React, { useEffect } from 'react';
 
-} from "ymap3-components";
+const MapComponent = () => {
+    useEffect(() => {
+        // Создаем элемент <script> для загрузки API
+        const script = document.createElement('script');
+        script.src = `https://api-maps.yandex.ru/v3/?apikey=${import.meta.env.VITE_YANDEX_MAP_API_KEY}Y&lang=ru_RU`;
+        script.async = true;
+        document.body.appendChild(script);
 
+        // После загрузки скрипта вызываем initMap
+        script.onload = () => {
+            initMap();
+        };
 
-function AdminYandexMap() {
+        // Функция инициализации карты
+        const initMap = async () => {
+            await ymaps3.ready;
+
+            const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+
+            const map = new YMap(
+                document.getElementById('map'),
+                {
+                    location: {
+                        center: [37.588144, 55.733842],
+                        zoom: 10
+                    }
+                }
+            );
+
+            map.addChild(new YMapDefaultSchemeLayer());
+        };
+
+        // Очищаем скрипт при размонтировании компонента
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []); // [] указывает на то, что useEffect выполнится только один раз при монтировании компонента
+
     return (
-        <YMapComponentsProvider apiKey={import.meta.env.VITE_YANDEX_MAP_API_KEY}>
-            <YMap location={{center: [25.229762, 55.289311], zoom: 9}} mode="vector">
-                <YMapDefaultSchemeLayer/>
-                <YMapDefaultFeaturesLayer/>
-
-                <YMapMarker coordinates={[25.229762, 55.289311]} draggable={true}>
-                    <section>
-                        <h1>You can drag this header</h1>
-                    </section>
-                </YMapMarker>
-            </YMap>
-        </YMapComponentsProvider>
+        <div id="map" style={{ width: '600px', height: '400px' }}></div>
     );
-}
+};
 
-export default AdminYandexMap;
+export default MapComponent;
