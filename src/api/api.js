4e1dda376b07e474ce54f9acb93eadb050426cbd
renat2006 <renatgubaudullin@gmail.com>
2024-07-19 20:34:17 +0300

    Fixed auth

--- src/api/api.js
+++ src/api/api.js
@@ -32,4 +32,4 @@ export const apiRequest = async (endpoint, method = 'GET', body = null, token = 
 export const authenticate = (telegramData) => apiRequest('/auth', 'POST', telegramData);
 export const refreshToken = (refreshToken) => apiRequest('/refresh', 'POST', null, refreshToken);
 export const saveProgress = (progressData, token) => apiRequest('/save_progress', 'PUT', progressData, token);
-export const get = (progressData) => apiRequest('/get', 'GET');
+export const get = (progressData) => apiRequest('/uuid', 'GET');
