ae25f52aabb396427a5659a2c9999437af288dc0
renat2006 <renatgubaudullin@gmail.com>
2024-07-30 21:25:12 +0300

    updated validator

--- src/forms/RouteInfo.jsx
+++ src/forms/RouteInfo.jsx
@@ -43,7 +43,7 @@ export default function RouteInfo({
                 .max(200, "Описание не может превышать 200 символов"),
             routeAudioTeaser: Yup.mixed()
                 .test('fileSize', 'Размер файла не должен превышать 10 МБ', value => !value || (value && value.size <= 10 * 1024 * 1024))
-                .test('fileType', 'Файл должен быть аудиофайлом', value => !value || (value && ['audio/mpeg', 'audio/wav', 'audio/ogg'].includes(value.type))),
+                .test('fileType', 'Файл должен быть аудиофайлом в формате (mp3, wav, ogg)', value => !value || (value && ['audio/mpeg', 'audio/wav', 'audio/ogg'].includes(value.type))),
         }),
         onSubmit: async (values) => {
             const toastId = toast.loading("Сохранение...");
