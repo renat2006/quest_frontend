b5e142a01cb53d69afc85ae6c9e8082f92ea9f12
renat2006 <renatgubaudullin@gmail.com>
2024-07-31 22:59:45 +0300

    fix

--- src/forms/RouteInfo.jsx
+++ src/forms/RouteInfo.jsx
@@ -24,8 +24,17 @@ export default function RouteInfo({
     const [selectedRouteLanguage, setSelectedRouteLanguage] = useState(routeLanguage || '');
     const [audioFile, setAudioFile] = useState(routeAudioTeaser || '');
     const [audioURL, setAudioURL] = useState(routeAudioTeaser ? URL.createObjectURL(routeAudioTeaser) : '');
+    console.log({
+        routeName,
+        routeType,
+        routeLanguage,
+        routeDescription,
+        routeAudioTeaser,
+        accessToken,
+        questId
 
-    const navigate = useNavigate();
+    })
+
 
     const formik = useFormik({
         initialValues: {
@@ -76,6 +85,8 @@ export default function RouteInfo({
     });
 
     useEffect(() => {
+        const navigate = useNavigate();
+        console.log(formik.values)
         formik.setFieldValue('routeType', selectedRouteType);
         formik.setFieldValue('routeLanguage', selectedRouteLanguage);
     }, [selectedRouteType, selectedRouteLanguage]);
