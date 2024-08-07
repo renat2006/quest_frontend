4c3c2f861647adcf775ae592c4cfdcb3969c26eb
renat2006 <renatgubaudullin@gmail.com>
2024-08-07 14:25:36 +0300

    prod build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import removeConsole from "vite-plugin-remove-console";
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), removeConsole({
-
-    exclude: ['error', 'warn']
-  })],
+  plugins: [react()],
 })
