6fb8f6d07883ff6638d71da22884d4aa624869a1
renat2006 <renatgubaudullin@gmail.com>
2024-08-22 12:35:48 +0300

    Added audio player, preloader, stories, media upload, location delete

--- src/componets/PlaceInfoModal/PlaceInfoModal.jsx
+++ src/componets/PlaceInfoModal/PlaceInfoModal.jsx
@@ -107,7 +107,7 @@ export default function QuestInfoModal({ isOpen, onOpenChange, point }) {
                 stories={stories}
                 defaultInterval={1500}
                 width="100%"
-                height="90vh"
+                height="75vh"
                 loop
                 keyboardNavigation
             />
